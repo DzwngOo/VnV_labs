81b2c37aa338c658d9d8425b91a4104991ac211f
DreamlessOo <D3nnisng1999@gmail.com>
2025-09-29 22:39:52 +0800

    .

--- ICT3101_Calculator.UnitTests/obj/Debug/net6.0/ICT3101_Calculator.UnitTests.AssemblyInfo.cs
+++ ICT3101_Calculator.UnitTests/obj/Debug/net6.0/ICT3101_Calculator.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICT3101_Calculator.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d04b74778684a8e3b4e92af1ceaeb89c3370a78")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICT3101_Calculator.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICT3101_Calculator.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
